b41fbbc99344c4334fc868ce20e881f8df90326e
tatatatarenko38@gmail.com <your_mail@example.com>
2023-10-24 20:43:55 +0300

    boots

--- src/route/index.js
+++ src/route/index.js
@@ -1093,6 +1093,16 @@ router.get('/facebook', function (req, res) {
 })
 
 // ================================================================
+router.get('/bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap', {
+    layout: 'bootstrap'
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 
 
 // Підключаємо роутер до бек-енду
