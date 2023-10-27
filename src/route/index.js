1c5227addc1687df0af97e93993e0b5f28519702
tatatatarenko38@gmail.com <your_mail@example.com>
2023-10-27 12:49:17 +0300

    b1

--- src/route/index.js
+++ src/route/index.js
@@ -1488,6 +1488,14 @@ router.get('/task31', function (req, res) {
 })
 
 // ===============================================================
+router.get('boots1/', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('boots1', {
+    layout:'basic',
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
